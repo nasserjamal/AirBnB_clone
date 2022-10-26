2a67530e795577960c983deab4f0193c53d3ac80
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-10-26 18:13:58 +0300

    Created BaseClass

---
+++ models/base_model.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Contains a BaseModel class"""
+
+
+import datetime
+import uuid
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """Instantiation method for our class"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    self.__dict__[key] = value
+
+    def __str__(self):
+        """String representation for our class"""
+
+        return "[{}] ({}) {}"\
+            .format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with the current datetime"""
+
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary of all keys/values of __dict__ of instance"""
+
+        self.__dict__['__class__'] = type(self).__name__
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        return self.__dict__
