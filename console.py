9dee227c40ed65f7358bb304a0e4ec081c5c259c
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2022-11-01 23:04:01 +0300

    stuck at show

--- console.py
+++ console.py
@@ -13,34 +13,27 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb)'
     file = None
-
+    
     def emptyline(self):
-        """Defines behaviour of console if empty line is enmtered"""
+        """Defines behaviour of console if empty line is entered"""
 
         pass
-
-    def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
 
     def do_quit(self, arg):
         """Command for exiting the console"""
 
-        self.close()
         return True
 
     def do_EOF(self, arg):
         """Command for exiting the console"""
 
-        self.close()
+        print("")
         return True
 
     def do_create(self, arg):
         """Creates a new instance of BaseModel class and saves it"""
 
         my_module_classes = list(x[0] for x in inspect.getmembers(base_model, inspect.isclass))
-        print(my_module_classes)
         if arg == "" :
             print("** class name missing **")
         elif arg not in my_module_classes:
@@ -49,6 +42,21 @@ class HBNBCommand(cmd.Cmd):
             self.arg = BaseModel()
             self.arg.save()
             print(self.arg.id)
+
+    def do_show(self, *args):
+        """Shows the string representation of the instance"""
+
+        my_module_classes = list(x[0] for x in inspect.getmembers(base_model, inspect.isclass))
+        if args[0] == "":
+           print("** class name missing **")
+        elif args[0] not in my_module_classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif args[1] not in self.__dict__.values():
+            print("** no instance found **")
+        
+
 
 
 if __name__ == '__main__':
