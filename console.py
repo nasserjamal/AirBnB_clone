270bb61f24ee11fba64909ada76f7f3cf07b8184
Nasser Jamal <nasserjamal30@gmail.com>
2022-11-05 15:08:39 +0300

    Fixed leading whitespace from filenames

--- console.py
+++ console.py
@@ -7,8 +7,8 @@ from shlex import split
 import models
 from models.base_model import BaseModel
 from models.user import User
-# from models.city import City
-# from models.amenity import Amenity
+from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.state import State
 from models.review import Review
